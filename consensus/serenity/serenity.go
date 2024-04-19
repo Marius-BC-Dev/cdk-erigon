39c2303370fc27a427bf04c9010fda153418bcad
marius <wangone2008@gmail.com>
2024-04-20 00:35:22 +0800

    Debug Finalize

--- consensus/serenity/serenity.go
+++ consensus/serenity/serenity.go
@@ -131,9 +131,11 @@ func (s *Serenity) Finalize(config *chain.Config, header *types.Header, state *s
 ) (types.Transactions, types.Receipts, error) {
 	log.Info("Serenity")
 	if !IsPoSHeader(header) {
+		log.Info("1")
 		return s.eth1Engine.Finalize(config, header, state, txs, uncles, r, withdrawals, chain, syscall)
 	}
 	if auraEngine, ok := s.eth1Engine.(*aura.AuRa); ok {
+		log.Info("2")
 		if err := auraEngine.ApplyRewards(header, state, syscall); err != nil {
 			return nil, nil, err
 		}
@@ -143,12 +145,15 @@ func (s *Serenity) Finalize(config *chain.Config, header *types.Header, state *s
 			}
 		}
 	} else {
+		log.Info("3")
 		for _, w := range withdrawals {
+			log.Info("31", "w", w)
 			amountInWei := new(uint256.Int).Mul(uint256.NewInt(w.Amount), uint256.NewInt(params.GWei))
 			state.AddBalance(w.Address, amountInWei)
 		}
 	}
 	if config.IsCancun(header.Time) {
+		log.Info("4")
 		parent := chain.GetHeaderByHash(header.ParentHash)
 		if parent == nil {
 			return nil, nil, fmt.Errorf("Could not find the parent of block %v to get excess data gas", header.Number.Uint64())
